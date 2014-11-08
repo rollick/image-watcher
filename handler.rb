ec0a444711f43e95561c759530f8abb386cd541a
rollick <mark@onepercentclub.com>
2014-11-08 23:33:53 +0100

    add hashing and clean up code to handle basic deleting of images

--- handler.rb
+++ handler.rb
@@ -1,42 +1,59 @@
 require "optparse"
 require "mongo"
 require "exifr"
+require "murmurhash3"
 
 
 options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: handler.rb [options]"
 
-  opts.on("-a", "--args [ARGUMENTS]", "Filename and flag arguments from fswatch") do |a|
+  opts.on("-a", "--args [ARGUMENTS]", "Filename and flag arguments from fswatch (| as separator)") do |a|
     options[:args] = a
   end
 end.parse!
 
-match = options[:args].match(/(^.*?)\s(\d+)$/i)
+parts = options[:args].split("|")
 
-unless match.length > 2
+unless parts.length > 2
     raise "Path and flag not provided!"
-end 
+end
 
-path = match[1]
-flag = match[2]
+path = parts[0]
+is_file = parts.include? "IsFile"
 
-# If flag is 558 then the file was created
-if flag == "528" && path.match(/\.jpg$/i)
-    p "Inserting new record..."
+if is_file && path.match(/\.(jpg|png|jpeg)$/i)
     include Mongo
 
     gallery = MongoClient.new("localhost", 27017).db("gallery")
     images = gallery.collection("images")
 
-    image = {
-        "path" => path
-    }
+    # If path doesn't exist then delete the record
+    if !File.exist?(path)
+        p "Deleting image (#{path})..."
+        
+        # TODO: How to remove the deleted photo without the hash?
+        #       Image path isn't very reliable.
+        images.remove({:path => path})
+    else
+        hash = MurmurHash3::V32.str_hash(File.read(path))
+        
+        # If image doesn't exist in db then insert
+        if images.find({:hash => hash}).count() == 0
+            p "Inserting image (#{path})..."
 
-    info = EXIFR::JPEG.new(path)
+            info = EXIFR::JPEG.new(path)
+            date_taken = info.date_time_digitized ? info.date_time_digitized : (info.date_time ? info.date_time : Time.now)
 
-    image["dateTaken"] = (info.exif? && info.date_time) ? info.date_time : Time.now
+            image = {
+                "path" => path,
+                "hash" => hash,
+                "dateTaken" => date_taken
+            }
 
-    p "=> #{image}"
-    images.insert(image)
+            images.insert(image)
+        else
+            p "Skipping existing image (#{path})..."
+        end
+    end
 end
